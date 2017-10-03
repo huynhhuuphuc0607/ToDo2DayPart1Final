7922ce3e877c7b36af01fe2464e713f166d2a8a7
phuynh101 <o06070701>
2017-10-03 16:55:45 -0700

    1/ Skills acquired: adater.notifyDatSetChanged(),  view.setTag( Object), onResume() 2/ Updates: + adds layouts + supports Add Task, Clear All Tasks

--- app/src/main/java/edu/orangecoastcollege/cs273/todo2day/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/todo2day/MainActivity.java
@@ -2,7 +2,12 @@ package edu.orangecoastcollege.cs273.todo2day;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,50 +17,78 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private List<Task> allTasksList = new ArrayList<>();
 
+    //Reference to the database
+    private DBHelper mDB;
+
+    //reference to the widgets needed
+    private EditText mDescriptionEditText;
+    private ListView mTaskListView;
+
+    //reference to the custom list adapter
+    TaskListAdapter mTaskListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        deleteDatabase(DBHelper.DATABASE_NAME);
+        mDescriptionEditText = (EditText) findViewById(R.id.taskEditText);
+        mTaskListView = (ListView) findViewById(R.id.taskListView);
+        mDB = new DBHelper(this);
 
-        allTasksList.add(new Task("Study for CS A273 Midterm", false));
-        allTasksList.add(new Task("Get that laundry done!", false));
-        allTasksList.add(new Task("Master explicit intents", true));
-        allTasksList.add(new Task("Play League of Legends", true));
+    }
 
-        DBHelper db = new DBHelper(this);
-        for (Task t : allTasksList)
-            db.addTask(t);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //database-related stuff
+        // 1) populate a list using db helper
+        allTasksList = mDB.getAllTasks();
+        // 2) connect the listview with the custom list
+        mTaskListAdapter = new TaskListAdapter(this,R.layout.task_item,allTasksList);
+        mTaskListView.setAdapter(mTaskListAdapter);
+    }
 
-        allTasksList = db.getAllTasks();
-        Log.i(TAG, "Displaying all Tasks after adding:");
-        for (Task t : allTasksList)
-            Log.i(TAG, t.toString());
+    public void addTask(View v) {
+        //WE SHOULD USE THE ID FROM THE DATABASE AFTER ADDING A NEW TASK TO THE DATABASE
+        //OTHERWISE, IF WE WE CREATE A NEW TASK, THE FIRST TIME WE TOGGLE THE TASK STATUS,
+        //IT WONT APPLY
 
-        db.deleteTask(allTasksList.get(2));
-        db.deleteTask(allTasksList.get(3));
-        allTasksList = db.getAllTasks();
-        Log.i(TAG, "Displaying all Tasks after deleting T3 and T4:");
-        for (Task t : allTasksList)
-            Log.i(TAG, t.toString());
+        //For the time being, we will just do a short way (Demonstrate right below)
 
+        //check to see if the description is empty or not
+        String description = mDescriptionEditText.getText().toString();
+        if (TextUtils.isEmpty(description))
+            Toast.makeText(this, "Please enter a description.", Toast.LENGTH_SHORT).show();
+        else
+        {
+            //create a task
+            Task newTask = new Task(description,false);
+            //Add it to the database
+            mDB.addTask(newTask);
+            //Add it to the list
+            allTasksList.add(newTask);
+            //Notify the list adapter that it's been changed
+            mTaskListAdapter.notifyDataSetChanged();
+            //Clear out the EditText
+            mDescriptionEditText.setText("");
+        }
+    }
 
-        Task updatedTask = allTasksList.get(0);
-        updatedTask.setDescription("Pretend to study for CS A273 Midterm");
-        updatedTask.setIsDone(true);
-        db.updateTask(updatedTask);
+    public void clearAllTasks(View v)
+    {
+        allTasksList.clear();
+        mDB.deleteAllTasks();
+        mTaskListAdapter.notifyDataSetChanged();
+    }
 
-        allTasksList = db.getAllTasks();
-        Log.i(TAG, "Displaying all Tasks after updating T1:");
-        for (Task t : allTasksList)
-            Log.i(TAG, t.toString());
-
-
-        Task t2 = db.getSingleTask(2);
-        Log.i(TAG, "Displaying Task T2:");
-        Log.i(TAG, t2.toString());
-
-        db.close();
+    public void changeTaskStatus(View v)
+    {
+        CheckBox selectedCheckBox = (CheckBox)v;
+        Task selectedTask = (Task) selectedCheckBox.getTag();
+        //update the task
+        selectedTask.setIsDone(selectedCheckBox.isChecked());
+        //update the database
+        mDB.updateTask(selectedTask);
     }
 }
+
